d64c496c5eb774a59fec72e2d8121ba9a5db211b
Ishmeet Kaur <ishiikaur004@gmail.com>
2026-08-19 20:38:55 +0530

    Time: 74 ms (54.54%), Space: 47 MB (19.5%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+
+        String trimmed =
+                doubled.substring(1, doubled.length() - 1);
+
+        return trimmed.contains(s);
+    }
+}
