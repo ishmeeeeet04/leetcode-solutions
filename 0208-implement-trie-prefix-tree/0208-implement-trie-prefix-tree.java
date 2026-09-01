2ce1f46d28ed2da9e74cd04a1069fc44b63ac3b1
Ishmeet Kaur <ishiikaur004@gmail.com>
2026-09-01 20:58:39 +0530

    Time: 31 ms (86.87%), Space: 61.9 MB (74.14%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,66 @@
+class Trie {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd;
+    }
+
+    TrieNode root = new TrieNode();
+
+    public Trie() {
+    }
+
+    public void insert(String word) {
+
+        TrieNode current = root;
+
+        for (char ch : word.toCharArray()) {
+
+            int index = ch - 'a';
+
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+
+            current = current.children[index];
+        }
+
+        current.isEnd = true;
+    }
+
+    public boolean search(String word) {
+
+        TrieNode current = root;
+
+        for (char ch : word.toCharArray()) {
+
+            int index = ch - 'a';
+
+            if (current.children[index] == null) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return current.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+
+        TrieNode current = root;
+
+        for (char ch : prefix.toCharArray()) {
+
+            int index = ch - 'a';
+
+            if (current.children[index] == null) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return true;
+    }
+}
